f5d23df21b10a93e2bd4cdf4d9d54909b5cc7415
Mike Jerred <mjerred.work@gmail.com>
2016-04-25 19:51:42 +0100

    Making header fan

--- client/views/default/header/header.ts
+++ client/views/default/header/header.ts
@@ -1,6 +1,17 @@
 ﻿module MJ.Views.Default.Header {
     class Controller {
         public show = true;
+        public expanded = false;
+
+        public expandClick() {
+            if (!this.show && this.expanded) {
+                // if the menu is expanded we dont want to hide the whole header
+                // before it gets collapsed
+                this.show = true;
+            }
+
+            this.expanded = !this.expanded;
+        }
     }
 
     export var view = {
