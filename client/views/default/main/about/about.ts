f1276a0c045ed84b6ece876b3b6c372f6263b111
Mike Jerred <mjerred.work@gmail.com>
2016-06-16 17:50:51 +0100

    Starting about page

--- client/views/default/main/about/about.ts
+++ client/views/default/main/about/about.ts
@@ -1,5 +1,11 @@
 module MJ.Views.Default.Main.About {
+    class AboutController {
+
+    }
+
     export var view = {
-        templateUrl: 'views/default/main/about/about.html'
+        templateUrl: 'views/default/main/about/about.html',
+        controller: AboutController,
+        controllerAs: 'aboutCtrl'
     };
 }
