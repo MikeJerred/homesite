cd10d3c5d97ae615ca52c0bca1d37521dfbe6014
Mike Jerred <mjerred.work@gmail.com>
2016-09-13 00:19:50 +0100

    Improving page-laoded directive

--- client/directives/page/loaded/page-loaded.ts
+++ client/directives/page/loaded/page-loaded.ts
@@ -4,17 +4,17 @@ module MJ.Directives.Page.Loaded {
         ($timeout: ng.ITimeoutService) => ({
             restrict: 'A',
             link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes) => {
-                let timeout = () => $timeout(
-                    () => {
-                        if (!$('.ui-view-animate').hasClass('ng-enter')) {
-                            element.addClass('page-loaded');
-                        } else {
-                            timeout();
-                        }
-                    },
-                    50);
-
-                timeout();
+                let transitionStarted = $('.ui-view-animate').length === 1;
+                let unwatch = scope.$watch(() => $('.ui-view-animate').hasClass('ng-enter'), inTransition => {
+                    if (!transitionStarted) {
+                        if (inTransition)
+                            transitionStarted = true;
+                    }
+                    else if (inTransition === false) {
+                        element.addClass('page-loaded');
+                        unwatch();
+                    }
+                });
             }
         })
     ]);
