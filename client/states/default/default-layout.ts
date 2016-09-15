572aa897e859a28c78662b145bb9c47cfd8f021d
Mike Jerred <mjerred.work@gmail.com>
2016-09-15 15:46:44 +0100

    Cleaning up, making ui-view animations better

--- client/states/default/default-layout.ts
+++ client/states/default/default-layout.ts
@@ -5,8 +5,9 @@ module MJ.States.Default {
             // detect whether a state change happened because the user:
             // 1. used the browser history (back/forward buttons): $stateChangeSuccess happens after $locationChangeSuccess
             // 2. navigated using links on the page: $stateChangeSuccess happens before $locationChangeSuccess
-            // Note: this is quite hacky as it is relying on the internal behavior of angular,
-            // perhaps it is cleaner to do it using window.onpopstate
+            // this is necessary because when using browser history the page should scroll to the last position,
+            // but when following links it should always take the user to the top of the page.
+            // Note: this is quite hacky as it is relying on the internal behavior of angular, perhaps it is cleaner to do it using window.onpopstate
             let lastNavigationEvent = 0;
             $scope.$on('$stateChangeStart', () => {
                 lastNavigationEvent = 0;
@@ -14,44 +15,41 @@ module MJ.States.Default {
             $scope.$on(
                 '$stateChangeSuccess',
                 (event: ng.IAngularEvent, toState: ng.ui.IState, toParams: any, fromState: ng.ui.IState, fromParams: any) => {
-                    if (lastNavigationEvent === 2)
-                        this.browserHistoryNavigate(toState, toParams, fromState, fromParams);
-
-                    lastNavigationEvent = 1;
-
                     if (fromState.name) {
                         // as we change view the fromState view will become position: fixed, so we need to offset its top by the
                         // current scroll position so that it remains in place. This is because the scroll position will soon
                         // change to that of the toState and we don't want the old view to be affected by that during the animation
                         let scrollPos = $window.scrollY;
                         $('.ui-view-animate').css('top', -scrollPos);
+                        this.$window.scrollTo(0, 0);
 
                         if (fromState.name === 'default.blog') {
                             // save data for the fromState so that we can use it if the user goes back to this state later on
                             if (!fromState.data)
                                 fromState.data = {};
 
-                            // we need this key because we might be transitioning to the same state just with different params
                             const key = this.getDataKey(fromParams);
-
-                            fromState.data[key] = {
-                                scrollY: scrollPos
-                            };
+                            fromState.data[key] = { scrollY: scrollPos };
                         }
 
                         this.setLeaveAnimation(toState, toParams, fromState, fromParams);
+
+                        let historyNavigate = lastNavigationEvent === 2;
                         let unbind = $scope.$on('$viewContentLoaded', () => {
                             this.setEnterAnimation(toState, toParams, fromState, fromParams);
+                            if (historyNavigate)
+                                this.browserHistoryNavigate(toState, toParams, fromState, fromParams);
+                            else
+                                this.linkNavigate();
+
                             unbind();
                         });
-                        this.$window.scrollTo(0, 0);
                     }
+
+                    lastNavigationEvent = 1;
                 }
             );
             $scope.$on('$locationChangeSuccess', () => {
-                if (lastNavigationEvent === 1)
-                    this.linkNavigate();
-
                 lastNavigationEvent = 2;
             });
         }
@@ -104,10 +102,6 @@ module MJ.States.Default {
                 const key = this.getDataKey(toParams);
                 const stateData = toState.data[key];
 
-                if (toState.name !== 'default.blog') {
-                    stateData.scrollY = 0;
-                }
-
                 // scroll to last known position, or to the top if we don't know the last position
                 this.scrollTo(stateData.scrollY || 0);
             } else {
@@ -121,9 +115,29 @@ module MJ.States.Default {
         }
 
         private scrollTo(y: number) {
-            let unbind = this.$scope.$on('$viewContentLoaded', () => {
-                this.$timeout(() => { $('html, body').animate({ scrollTop: y }, 200); }, 500);
-                unbind();
+            $('.ui-view-animate-container').css({
+                'height': '100vh',
+                'overflow': 'hidden'
+            });
+            let innerArticle = $('.ui-view-animate > article').filter(index => index === 0);
+            innerArticle.css({
+                'top': -y,
+                'position': 'absolute',
+                'width': '100%'
+            });
+
+            let viewAnimData = $('.ui-view-animate').filter(index => index === 0).data('$uiViewAnim');
+            viewAnimData.$animEnter.then(() => {
+                $('.ui-view-animate-container').css({
+                    'height': 'auto',
+                    'overflow': 'visible'
+                });
+                innerArticle.css({
+                    'top': 'auto',
+                    'position': 'static',
+                    'width': 'auto'
+                });
+                this.$window.scrollTo(0, y);
             });
         }
     }
