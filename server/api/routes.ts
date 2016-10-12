a12a6e92a358ff1c77604ff027dd376aa704e613
Michael Jerred <mjerred.work@gmail.com>
2016-10-12 10:37:03 +0100

    Updating some packages

--- server/api/routes.ts
+++ server/api/routes.ts
@@ -1,9 +1,9 @@
+import * as router from './router';
+
 const controllers = require('require-all')({
     dirname     :  __dirname,
     filter      :  /(.+Controller)\.js$/,
     recursive   : true
 });
-
-const router = require('./router');
 
 export default router.default;
