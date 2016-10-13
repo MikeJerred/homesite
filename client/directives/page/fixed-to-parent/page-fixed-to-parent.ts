1f43acb7455ad218bf6d2d5f72bd383e9d7e58ab
Michael Jerred <mjerred.work@gmail.com>
2016-10-13 11:45:08 +0100

    Fixes #8 - adding fix for firefox jumping fixed iamge bug

---
+++ client/directives/page/fixed-to-parent/page-fixed-to-parent.ts
@@ -0,0 +1,32 @@
+module MJ.Directives.Page.PageFixedToParent {
+    angular.module('mj.directives').directive('pageFixedToParent', [
+        '$window',
+        ($window: ng.IWindowService) => ({
+            restrict: 'E',
+            link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes) => {
+                let parent = element.parent();
+                scope.$watch(() => parent.outerHeight(), (val: number) => {
+                    element.height(val);
+                });
+                scope.$watch(() => parent.outerWidth(), (val: number) => {
+                    element.width(val);
+                });
+
+                scope.$watchGroup(
+                    [
+                        () => parent[0].getBoundingClientRect().top,
+                        () => parent[0].getBoundingClientRect().bottom
+                    ],
+                    (values: number[]) => {
+                        if (values[1] > 0 && values[0] < $window.innerHeight) {
+                            element.css('display', 'block');
+                        } else {
+                            element.css('display', 'none');
+                        }
+                    }
+                );
+            },
+            template: ''
+        })
+    ]);
+}
