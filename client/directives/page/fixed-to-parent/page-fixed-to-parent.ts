d9ba14a427551ce3bf4b4f6f129bd744ff1fbd5a
Michael Jerred <mjerred.work@gmail.com>
2016-10-14 17:08:16 +0100

    Adding hack :(

--- client/directives/page/fixed-to-parent/page-fixed-to-parent.ts
+++ client/directives/page/fixed-to-parent/page-fixed-to-parent.ts
@@ -1,24 +1,31 @@
 module MJ.Directives.Page.PageFixedToParent {
+    // creates an element that has position fixed but takes on the size of its parent element
     angular.module('mj.directives').directive('pageFixedToParent', [
-        '$window',
-        ($window: ng.IWindowService) => ({
+        '$window', '$timeout',
+        ($window: ng.IWindowService, $timeout: ng.ITimeoutService) => ({
             restrict: 'E',
             link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes) => {
                 let parent = element.parent();
-                scope.$watch(() => parent.outerHeight(), (val: number) => {
-                    element.height(val);
-                });
-                scope.$watch(() => parent.outerWidth(), (val: number) => {
-                    element.width(val);
-                });
 
-                scope.$watchGroup(
-                    [
-                        () => parent[0].getBoundingClientRect().top,
-                        () => parent[0].getBoundingClientRect().bottom
-                    ],
-                    (values: number[]) => {
-                        if (values[0] < ($window.innerHeight + 100) && values[1] > -100) {
+                // hack: timeout here to prevent angular reaching 10 digest cycles in release mode.
+                // this seems to be to do with the critical html causing the height & width to change rapidly.
+                $timeout(() => {
+                    scope.$watchGroup(
+                        [
+                            () => parent.outerHeight(),
+                            () => parent.outerWidth()
+                        ],
+                        (values: number[]) => {
+                            element.height(values[0]);
+                            element.width(values[1]);
+                        });
+                }, 300);
+
+                scope.$watch(
+                    () => parent[0].getBoundingClientRect().top < ($window.innerHeight + 100)
+                        && parent[0].getBoundingClientRect().bottom > -100,
+                    (visible) => {
+                        if (visible) {
                             element.css('display', 'block');
                         } else {
                             element.css('display', 'none');
