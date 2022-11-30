c1fbeffc4e3244ed56179d1e5dfe322f4bc5ef7a
Mike Jerred <mjerred.work@gmail.com>
2022-11-30 18:26:22 +0000

    update to angular 15

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,8 +21,7 @@ const routes: Routes = [
         PortfolioModule,
         RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules,
-    scrollPositionRestoration: 'disabled',
-    relativeLinkResolution: 'legacy'
+    scrollPositionRestoration: 'disabled'
 })
     ],
     exports: [ RouterModule ]
